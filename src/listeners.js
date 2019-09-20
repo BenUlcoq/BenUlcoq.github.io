af74ce037523095fc8e99aac1dcdb585f128d8ad
Ben Ulcoq <ben.ulcoq@gmail.com>
2019-09-20 16:58:50 +1000

    Updated Layouts

--- src/listeners.js
+++ src/listeners.js
@@ -7,8 +7,10 @@ $(document).ready(function () {
 
    $(window).scroll(function () {
 
-    // var element = document.getElementById("chevron-container");
-    // element.classList.add("scrolled");
+    if ($("body").hasClass("homepage")) {
+    var element = document.getElementById("chevron-container");
+    element.classList.add("scrolled");
+    }
 
       var a = $(window).scrollTop();
       var b = navbar.height();
@@ -27,24 +29,50 @@ $(document).ready(function () {
 
 $(function() {
   $('.card').click(function() {
+    $(this).css("transform","scale(1)");
+    $('.dark').toggleClass('hidden')
+    $('.projects-wrapper').toggleClass('hidden')
     $('.card').not(this).removeClass('open')
     $('.card').not(this).toggleClass('hidden')
     $(this).toggleClass('open');
-    $('.overlay').toggleClass('on');
     $('header').toggle();
     $('footer').toggle();
+    $('.page-title').toggleClass('hidden');
     $(this).siblings('h2').toggleClass('hidden');
-
-
   })
   
 })
 
+$("#open-navmenu").keyup(function(event) {
+  if (event.keyCode === 13) {
+      $("#id_of_button").click();
+  }
+});
+
+$("#close-navmenu").keyup(function(event) {
+  if (event.keyCode === 13) {
+      $("#id_of_button").click();
+  }
+});
+
+
 $(function() {
-  $('.mobile-nav i').click(function(){
-    // $(this).find('i').toggle('fa-bars fa-times')
-    $('.mobile-nav').find('i').toggle('fa-bars fa-times')
-    $('.mobile-nav').toggleClass('clicked')
+  $('#open-navmenu').click(function(){
+    $('#open-navmenu').toggle();
+    $('#close-navmenu').toggle();
+    $('#close-navmenu').focus();
+    $('.mobile-nav').toggleClass('clicked');
     $('.mobile-nav').find('li').toggle();
+});
+
+
+$(function() {
+  $('#close-navmenu').click(function(){
+    $('#close-navmenu').toggle();
+    $('#open-navmenu').toggle();
+    $('#open-navmenu').focus();
+    $('.mobile-nav').toggleClass('clicked');
+    $('.mobile-nav').find('li').toggle();
+});
 });
 });
